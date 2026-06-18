146064ab3954a2067bd69486463b0949ea0a6867
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2026-06-18 15:24:30 +0200

    cs_clone: short-circuit validation if resource was provider-generated
    Validation will always fail for provider-generated resource otherwise,
    as validation for "either primitive or group must be set" always fails,
    as they are not set as intended (`should`) state.
    
    Fixes resource discovery (e.g. via "puppet resource cs_clone")
    and hence also pruning of unmanaged resources.
    
    Signed-off-by: Oliver Freyermuth <freyermuth@physik.uni-bonn.de>

--- lib/puppet/type/cs_clone.rb
+++ lib/puppet/type/cs_clone.rb
@@ -143,6 +143,9 @@ Puppet::Type.newtype(:cs_clone) do
   validate do
     return if self[:ensure] == :absent
 
+    # If resource was generated dynamically by the provider (e.g., puppet resource or purge), short-circuit validation.
+    return if @sensitive_parameters.nil? || (provider && (provider.instance_variable_get(:@property_hash) rescue {}).any?)
+
     mandatory_single_properties = %i[primitive group]
     has_should = mandatory_single_properties.select { |prop| should(prop) }
     raise Puppet::Error, "You cannot specify #{has_should.join(' and ')} on this type (only one)" if has_should.length > 1
